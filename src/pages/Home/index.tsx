b00c43eeb8f04d9a883dc4f76cd7e66c3ebee527
Paulo Braz <paulobrazaraujo@gmail.com>
2021-03-06 12:53:17 -0300

    initial commit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,19 +14,19 @@ const Home: React.FC = () => {
     <Container>
       <h1>Contato</h1>
       <div>
-        <a href="https://github.com/paulobr4z" target="_blank">
+        <a href="https://github.com/paulobr4z" target="_blank" rel="noreferrer">
           <AiFillGithub style={{ fontSize:40 }} />
         </a>
-        <a href="https://www.linkedin.com/in/paulobr4z/" target="_blank">
+        <a href="https://www.linkedin.com/in/paulobr4z/" target="_blank" rel="noreferrer">
           <AiFillLinkedin style={{ fontSize:40 }} />
         </a>
-        <a href="mailto:paulobrazaraujo@gmail.com" target="_blank">
+        <a href="mailto:paulobrazaraujo@gmail.com" target="_blank" rel="noreferrer">
           <AiOutlineMail style={{ fontSize:40 }} />
         </a>
-        <a href="https://wa.me/5594981147978" target="_blank">
+        <a href="https://wa.me/5594981147978" target="_blank" rel="noreferrer">
           <AiOutlineWhatsApp style={{ fontSize:40 }} />
         </a>
-        <a href="https://www.instagram.com/paulobr4z/" target="_blank">
+        <a href="https://www.instagram.com/paulobr4z/" target="_blank" rel="noreferrer">
           <AiOutlineInstagram style={{ fontSize:40 }} />
         </a>
       </div>
