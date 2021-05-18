879b3b39bc3b245dcd4c50fa042b07d330eb4b99
Paulo Braz <paulobrazaraujo@gmail.com>
2021-05-18 01:21:53 -0300

    profile component style

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,37 +1,60 @@
-import React from 'react';
+import AvatarImg from '../../assets/avatar.jpg'
+
 import {
   AiFillGithub,
   AiFillLinkedin,
   AiOutlineMail,
-  AiOutlineWhatsApp,
   AiOutlineInstagram,
+  AiOutlineTwitter
 } from 'react-icons/ai';
 
-import { Container } from './styles';
+import {
+  Container,
+  Wrapper,
+  AvatarContainer,
+  Title
+} from './styles';
 
-const Home: React.FC = () => {
+export function Home() {
   return (
     <Container>
-      <h1>Contato</h1>
-      <div>
+      <Wrapper>
+        <AvatarContainer>
+          <img src={AvatarImg} alt="" />
+        </AvatarContainer>
+
+        <h4>
+          Paulo Braz
+        </h4>      
+
+        <h5>
+          Tucuruí-Pa
+        </h5>        
+
         <a href="https://github.com/paulobr4z" target="_blank" rel="noreferrer">
           <AiFillGithub style={{ fontSize:40 }} />
+          <Title>Github</Title>
         </a>
         <a href="https://www.linkedin.com/in/paulobr4z/" target="_blank" rel="noreferrer">
           <AiFillLinkedin style={{ fontSize:40 }} />
+          <Title>Linkedin</Title>
         </a>
         <a href="mailto:paulobrazaraujo@gmail.com" target="_blank" rel="noreferrer">
           <AiOutlineMail style={{ fontSize:40 }} />
-        </a>
-        <a href="https://wa.me/5594981147978" target="_blank" rel="noreferrer">
-          <AiOutlineWhatsApp style={{ fontSize:40 }} />
+          <Title>E-mail</Title>
         </a>
         <a href="https://www.instagram.com/paulobr4z/" target="_blank" rel="noreferrer">
           <AiOutlineInstagram style={{ fontSize:40 }} />
+          <Title>Instagram</Title>
         </a>
-      </div>
+        <a href="https://twitter.com/paulobr4z/" target="_blank" rel="noreferrer">
+          <AiOutlineTwitter style={{ fontSize:40 }} />
+          <Title>Twitter</Title>
+        </a>
+
+      </Wrapper>
+
+
     </Container>
   );
 };
-
-export default Home;
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,37 +4,72 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   height: 100vh;
-  background-color: #202124;
   color: white;
+  background: #155799;
+background: -webkit-linear-gradient(to right, #155799, #0082c8, #0082c8, #155799);
+background: linear-gradient(to right, #155799, #0082c8, #0082c8, #155799);
 
-  h1 {
-    margin-bottom: 35px;
+
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  flex-direction: column;
+  max-width: 560px; 
+  padding: 20px;
+
+  h4 {
+    margin-top: 20px;
+    margin-bottom: 10px;
   }
 
-  div {
+  h5 {
+    margin-bottom: 40px;
+  }
+
+  a {
     display: flex;
     align-items: center;
-    justify-content: space-between;
-    width: 320px;
-    padding: 14px;
-    /* border: 1px solid white; */
+    width: 100%;
+    border-radius: 50px;
+    border: 1px solid white;
+    margin: 16px 0px;
+    padding: 8px 8px;
+    color: white;
+    text-decoration: none;
+    font-weight: bold;
 
-    a {
-      color: white;
-    }
-
-    svg {
-      cursor: pointer;
-    }
-
-    svg:hover {
-      filter: brightness(50%);
+    &:hover {
+      background-color: white;
+      color: #292E49;
+      transition: all .3s linear;
     }
   }
+`;
 
+export const AvatarContainer = styled.div`
+  height: 120px;
+  width: 120px;
+  border-radius: 50%;
+  border: 3px solid white;
+  margin-top: 40px;
 
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    border-radius: 50%;
+  }
+`;
 
-
+export const Title = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  text-decoration: none;
 `;
